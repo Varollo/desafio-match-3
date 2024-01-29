c6c48d2e458744369e17fb0ed9ff3f2c9f2a6024
Gabriel Carollo <rimoldi98@gmail.com>
2024-01-29 18:47:14 -0300

    Added script to display score on the top left

---
+++ Assets/Script/ScoreDisplay.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private string scoreFormat = "{0}";
+    [Space]
+    [SerializeField]private GameHandler gameHandler;
+
+    private void Start() => gameHandler = gameHandler != null ? gameHandler : FindObjectOfType<GameHandler>();
+    private void OnEnable() => gameHandler.AddScoreListener(Refresh);
+    private void OnDisable() => gameHandler.RemoveScoreListener(Refresh);
+
+    public void Refresh(int newScore)
+    {
+        scoreText.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        scoreText.transform.DOKill();
+        scoreText.transform.DOScale(1, .5f);
+
+        scoreText.text = string.Format(scoreFormat, newScore);
+    }
+}
