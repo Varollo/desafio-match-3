467c3c3344952d19f35755636252c3e349db4827
Gabriel Carollo <rimoldi98@gmail.com>
2024-01-29 18:36:37 -0300

    Created ScoreManager object to keep track of player progress

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class ScoreManager
+{
+    public delegate void ScoreChangedDelegate(int newScore);
+    public event ScoreChangedDelegate OnScoreChanged;
+
+    private int score;
+
+    public int Score
+    {
+        get => score;
+        set
+        {
+            score = value;
+            OnScoreChanged?.Invoke(score);
+        }
+    }
+
+    public void Reset(int defaultScore = 0) => Score = defaultScore;    
+}
