f7d5050aa1ff8c53b63848457ccde6b8a280ff80
Gabriel Carollo <rimoldi98@gmail.com>
2024-01-30 23:37:19 -0300

    Added callbacks to TileView when tile moves or is destroyed

--- Assets/Script/TileView.cs
+++ Assets/Script/TileView.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TileView : MonoBehaviour
 {
+    /// <summary>
+    /// Called when tile moves as a result of a chain reaction, NOT when swapped.
+    /// </summary>
+    public virtual Tween OnTileMove(MovedTileInfo moveInfo) => TweenUtils.GetBlankTween();
+
+    /// <summary>
+    /// Called when tile is destroyed as a result of a match, when score is computed.
+    /// </summary>
+    public virtual Tween OnTileDestroyed() => TweenUtils.GetBlankTween();
 }
