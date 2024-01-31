be156b913b56e351b191c7f13af6a1369e43ddc2
Gabriel Carollo <rimoldi98@gmail.com>
2024-01-30 23:36:48 -0300

    Added utils class to centralize common methods

---
+++ Assets/Script/TweenUtils.cs
@@ -0,0 +1,9 @@
+﻿using DG.Tweening;
+
+public static class TweenUtils
+{
+    public static Tween GetBlankTween(float delaySec = 0.2f)
+    {
+        return DOVirtual.DelayedCall(delaySec, () => { });
+    }
+}
